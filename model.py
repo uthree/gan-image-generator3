66d390f1e084f75e1b17eb49263ffbe1a011f778
uthree <33905956+uthree@users.noreply.github.com>
2022-01-04 18:15:29 +0900

    fix

--- model.py
+++ model.py
@@ -6,9 +6,9 @@ import torch.optim as optim
 
 class Conv2dMod(nn.Module):
     """Some Information about Conv2dMod"""
-    def __init__(self, input_channels, output_channels, kernel_size, eps=1e-8):
+    def __init__(self, input_channels, output_channels, kernel_size=3, eps=1e-8):
         super(Conv2dMod, self).__init__()
-        self.weight = nn.Parameter(torch.randn(output_channels, input_channels, kernel_size, kernel_size, dtype=torch.float16))
+        self.weight = nn.Parameter(torch.randn(output_channels, input_channels, kernel_size, kernel_size))
         nn.init.xavier_uniform_(self.weight) # initialize weight
         self.input_channels = input_channels
         self.output_channels = output_channels
@@ -33,28 +33,69 @@ class Conv2dMod(nn.Module):
         weight = weight * d
         # weight: (batch_size, output_channels, input_channels, kernel_size, kernel_size)
         
-        # convolution
+        # reshape
         x = x.reshape(1, -1, H, W)
         _, _, *ws = weight.shape
         weight = weight.reshape(self.output_channels * N, *ws)
-        x = F.conv2d(x, weight, stride=1, padding=1, groups=N)
+        
+        
+        # padding
+        x = F.pad(x, (self.kernel_size // 2, self.kernel_size // 2, self.kernel_size // 2, self.kernel_size // 2), mode='replicate')
+        
+        # convolution
+        x = F.conv2d(x, weight, stride=1, padding=0, groups=N)
         x = x.reshape(N, self.output_channels, H, W)
 
         return x
-
-class Bias2d(nn.Module):
-    """Some Information about Bias2d"""
-    def __init__(self, channel, height, width):
-        super(Bias2d, self).__init__()
-        self.bias = nn.Parameter(torch.randn(channel, height, width, dtype=torch.float16))
-    def forward(self, x):
-        return x + self.bias
     
-class NoiseInjection(nn.Module):
-    """Some Information about NoiseInjection"""
-    def __init__(self):
-        super(NoiseInjection, self).__init__()
-        self.gain = nn.Parameter(torch.ones((), dtype=torch.float16))
-    def forward(self, x):
-        return x + torch.randn_like(x) * self.gain
+class Noise(nn.Module):
+    """Some Information about Noise"""
+    def __init__(self, channels):
+        super(Noise, self).__init__()
+        self.noise = nn.Parameter(torch.randn(channels))
 
+    def forward(self, x):
+        # x: (batch_size, channels, H, W)
+        # self.noise: (channels)
+        
+        # add noise
+        noise = self.noise[None, :, None, None]
+        noise = noise.expand(x.shape[0], *noise.shape)
+        x = x + noise
+        return x
+
+class ToRGB(nn.Module):
+    """Some Information about ToRGB"""
+    def __init__(self,channels):
+        super(ToRGB, self).__init__()
+        self.conv = nn.Conv2d(channels, 3, kernel_size=1, stride=1, padding=0)
+    def forward(self, x):
+        return self.conv(x)
+
+class GeneratorBlock(nn.Module):
+    """Some Information about GeneratorBlock"""
+    def __init__(self, input_channels, latent_channels, output_channels, style_dim):
+        super(GeneratorBlock, self).__init__()
+        self.affine1 = nn.Linear(style_dim, latent_channels)
+        self.conv1 = Conv2dMod(input_channels, latent_channels)
+        self.noise1 = Noise(latent_channels)
+        self.activation1 = nn.LeakyReLU()
+        
+        self.affine2 = nn.Linear(style_dim, output_channels)
+        self.conv2 = Conv2dMod(latent_channels, output_channels)
+        self.noise2 = Noise(output_channels)
+        self.activation1 = nn.LeakyReLU()
+        
+        self.to_rgb = ToRGB(output_channels)
+    def forward(self, x):
+        x = self.conv1(x, self.affine1(x))
+        x = self.noise1(x)
+        x = self.activation1(x)
+        
+        x = self.conv2(x, self.affine2(x))
+        x = self.noise2(x)
+        x = self.activation1(x)
+        rgb = self.to_rgb(x)
+        
+        return x, rgb
+    
